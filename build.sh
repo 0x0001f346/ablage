5689189c8c3b86b4f66b0c15f2d775965cfa9a80
Andreas Schulte <0x0001f346@pm.me>
2025-09-04 21:09:11 +0200

    feat: added android/arm64 build target

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-GOOS=windows GOARCH=amd64 go build -o build/ablage-windows-amd64.exe .
+GOOS=android GOARCH=arm64 go build -o build/ablage-android-arm64 .
 GOOS=linux GOARCH=amd64 go build -o build/ablage-linux-amd64 .
+GOOS=windows GOARCH=amd64 go build -o build/ablage-windows-amd64.exe .
